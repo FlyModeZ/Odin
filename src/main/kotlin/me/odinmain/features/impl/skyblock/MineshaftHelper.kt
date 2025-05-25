cbbc1e82f86e3d1e9751a1e898ae1f3ce2a10f88
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-05-25 16:59:03 +0800

    Update MineshaftHelper.kt
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/features/impl/skyblock/MineshaftHelper.kt
+++ src/main/kotlin/me/odinmain/features/impl/skyblock/MineshaftHelper.kt
@@ -3,9 +3,29 @@ package me.odinmain.features.impl.skyblock
 import me.odinmain.features.Module
 import me.odinmain.features.settings.impl.BooleanSetting
 
+import me.odinmain.utils.skyblock.LocationUtils.currentArea
+import me.odinmain.utils.skyblock.Island
+import me.odinmain.utils.skyblock.modMessage
+
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
+import net.minecraft.network.play.server.S3EPacketTeams
+
 object MineshaftHelper : Module(
     name = "Mineshaft",
     desc = "Announce party when you enter a mineshaft."
 ) {
-	private val noUse by BooleanSetting("Lapis Count Only", false, desc = "only show lapis cropse count")
+    private val noUse by BooleanSetting("Lapis Count Only", false, desc = "only show lapis cropse count")
+    @SubscribeEvent()
+    fun onPacket(event: PacketEvent.Receive) {
+        when (event.packet) {
+            is S3EPacketTeams -> {
+                if (!currentArea.isArea(Island.Mineshaft) || event.packet.action != 2) return
+                val text = event.packet.prefix.plus(event.packet.suffix).noControlCodes
+                variantRegex.find(text)?.groupValues?.get(1)?.let { modMessage(it) }
+            }
+        }
+    }
+    companion object {
+        private val variantRegex = Regex("^\\d\\d/\\d\\d/\\d\\d .+ (\\w{4}\\d)$")
+    }
 }
