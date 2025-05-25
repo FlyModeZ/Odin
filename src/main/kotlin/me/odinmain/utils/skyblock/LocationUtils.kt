5fca6f9ce9fe9097cf2dbcf236c5a82cf8cac63e
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-05-25 12:59:47 +0800

    qwq
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/utils/skyblock/LocationUtils.kt
+++ src/main/kotlin/me/odinmain/utils/skyblock/LocationUtils.kt
@@ -104,15 +104,8 @@ object LocationUtils {
             is S3DPacketDisplayScoreboard ->
                 modMessage("${event.packet.func_149370_d()}§r, ${event.packet.func_149371_c()}")
 
-            is S3EPacketTeams -> {
-                val clazz = S3EPacketTeams::class.java
-                val methods = clazz.declaredMethods
-                for (method in methods) {
-                    val parameterTypes = method.parameterTypes
-                    val returnType = method.returnType
-                    modMessage("Method: ${method.name} | ${parameterTypes.joinToString(", ") { it.simpleName }} | ${returnType.simpleName}")
-                }
-            }
+            is S3EPacketTeams -> 
+                modMessage("${event.packet.func_149307_h()}§r, ${event.packet.func_149309_f()}§r, ${event.packet.func_149311_e()}§r, ${event.packet.func_149308_i()}§r, ${event.packet.func_149306_d()}§r, ${event.packet.func_179814_i()}§r, ${event.packet.func_149310_g()}§r, ${event.packet.func_179813_h()}§r, ${event.packet.func_149312_c()}")
         }
     }
 }
