677f80f8e0c975eeaa7111dfe406426d79521fca
FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>
2025-05-25 17:11:25 +0800

    Update MineshaftHelper.kt
    Signed-off-by: FlyModeZ <102896994+FlyModeZ@users.noreply.github.com>

--- src/main/kotlin/me/odinmain/features/impl/skyblock/MineshaftHelper.kt
+++ src/main/kotlin/me/odinmain/features/impl/skyblock/MineshaftHelper.kt
@@ -2,13 +2,11 @@ package me.odinmain.features.impl.skyblock
 
 import me.odinmain.features.Module
 import me.odinmain.features.settings.impl.BooleanSetting
-
+import me.odinmain.utils.noControlCodes
 import me.odinmain.utils.skyblock.LocationUtils.currentArea
 import me.odinmain.utils.skyblock.Island
 import me.odinmain.utils.skyblock.modMessage
-
 import me.odinmain.events.impl.PacketEvent
-
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import net.minecraft.network.play.server.S3EPacketTeams
 
