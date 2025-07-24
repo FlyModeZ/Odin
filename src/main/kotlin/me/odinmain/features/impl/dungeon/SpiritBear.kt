07cf66b79ead0583d07687a9d68e0ca6d432b08c
Fly <flymodez@foxmail.com>
2025-07-24 16:48:38 +0800

    haveD try

--- src/main/kotlin/me/odinmain/features/impl/dungeon/SpiritBear.kt
+++ src/main/kotlin/me/odinmain/features/impl/dungeon/SpiritBear.kt
@@ -1,17 +1,27 @@
 package me.odinmain.features.impl.dungeon
 
+
 import me.odinmain.events.impl.BlockChangeEvent
 import me.odinmain.features.Module
+import me.odinmain.features.settings.impl.BooleanSetting
 import me.odinmain.features.settings.impl.HudSetting
+import me.odinmain.utils.offset
 import me.odinmain.utils.skyblock.dungeon.DungeonUtils
 import me.odinmain.utils.render.mcTextAndWidth
+import me.odinmain.utils.render.Renderer
 import me.odinmain.utils.toFixed
 import me.odinmain.utils.ui.Colors
+import net.minecraft.entity.monster.EntityGhast
 import net.minecraft.init.Blocks
 import net.minecraft.network.play.server.S32PacketConfirmTransaction
+import net.minecraft.util.AxisAlignedBB
 import net.minecraft.util.BlockPos
+import net.minecraftforge.client.event.RenderWorldLastEvent
+import net.minecraftforge.event.entity.EntityJoinWorldEvent
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 import java.util.HashSet
+
+import me.odinmain.utils.skyblock.modMessage
 
 object SpiritBear : Module(
     name = "Spirit Bear",
@@ -28,15 +38,19 @@ object SpiritBear : Module(
             mcTextAndWidth("§6Spirit Bear: $text", 0f, 0f, 1f, Colors.WHITE, center = false) + 2f to 12f
         } ?: (0f to 0f)
     }
+    private val boxSpawn by BooleanSetting("Box Spawn", true, desc = "早上好中国现在我有冰淇淋")
 
+    private val boxLocation = AxisAlignedBB(5.8, 70.8, 5.8, 5.3, 69.0, 5.3)
+    private val lastBlockLocation = BlockPos(7, 77, 34)
     private val f4BlockLocations = hashSetOf(BlockPos(-3, 77, 33), BlockPos(-9, 77, 31), BlockPos(-16, 77, 26), BlockPos(-20, 77, 20), BlockPos(-23, 77, 13), BlockPos(-24, 77, 6), BlockPos(-24, 77, 0), BlockPos(-22, 77, -7), BlockPos(-18, 77, -13), BlockPos(-12, 77, -19), BlockPos(-5, 77, -22), BlockPos(1, 77, -24), BlockPos(8, 77, -24), BlockPos(14, 77, -23), BlockPos(21, 77, -19), BlockPos(27, 77, -14), BlockPos(31, 77, -8), BlockPos(33, 77, -1), BlockPos(34, 77, 5), BlockPos(33, 77, 12), BlockPos(31, 77, 19), BlockPos(27, 77, 25), BlockPos(20, 77, 30), BlockPos(14, 77, 33), BlockPos(7, 77, 34))
     private val m4BlockLocations = hashSetOf(BlockPos(-2, 77, 33), BlockPos(-7, 77, 32), BlockPos(-13, 77, 28), BlockPos(-17, 77, 24), BlockPos(-21, 77, 18), BlockPos(-23, 77, 13), BlockPos(-24, 77, 7), BlockPos(-24, 77, 2), BlockPos(-23, 77, -4), BlockPos(-21, 77, -9), BlockPos(-17, 77, -14), BlockPos(-12, 77, -19), BlockPos(-6, 77, -22), BlockPos(-1, 77, -23), BlockPos(5, 77, -24), BlockPos(10, 77, -24), BlockPos(16, 77, -22), BlockPos(21, 77, -19), BlockPos(27, 77, -15), BlockPos(30, 77, -10), BlockPos(32, 77, -5), BlockPos(34, 77, 1), BlockPos(34, 77, 7), BlockPos(33, 77, 12), BlockPos(31, 77, 18), BlockPos(28, 77, 23), BlockPos(23, 77, 28), BlockPos(18, 77, 31), BlockPos(12, 77, 33), BlockPos(7, 77, 34))
-    private val lastBlockLocation = BlockPos(7, 77, 34)
     private inline val blockLocations: HashSet<BlockPos> get() = if (DungeonUtils.floor?.isMM == true) m4BlockLocations else f4BlockLocations
     private inline val maxKills: Int get() = if (DungeonUtils.floor?.isMM == true) 30 else 25
 
     private var kills = 0
     private var timer = -1 // state: -1=NotSpawned, 0=Alive, 1+=Spawning
+    private var boss: EntityGhast? = null
+    private var box = boxLocation
 
     init {
         onPacket<S32PacketConfirmTransaction> { if (timer > 0) timer -- }
@@ -50,6 +64,7 @@ object SpiritBear : Module(
             event.updated.block == Blocks.sea_lantern && event.old.block == Blocks.coal_block -> {
                 if (kills < maxKills) kills ++
                 if (event.pos == lastBlockLocation) timer = 68
+                modMessage("${boss?.positionVector}, ")
             }
 
             event.updated.block == Blocks.coal_block && event.old.block == Blocks.sea_lantern -> {
@@ -57,5 +72,19 @@ object SpiritBear : Module(
                 if (event.pos == lastBlockLocation) timer = -1
             }
         }
+    }
+
+    @SubscribeEvent
+    fun onEntityJoin(event: EntityJoinWorldEvent) {
+        if (!DungeonUtils.isFloor(4) || !DungeonUtils.inBoss) return
+        if (event.entity.name.contains("Bear")) modMessage("${event.entity.positionVector.xCoord}, ${event.entity.positionVector.yCoord}, ${event.entity.positionVector.zCoord}")
+        boss = event.entity as? EntityGhast ?: return
+        modMessage("found thorn uwu")
+    }
+
+    @SubscribeEvent
+    fun onRenderWorld(event: RenderWorldLastEvent) {
+        if (!DungeonUtils.isFloor(4) || !DungeonUtils.inBoss || !boxSpawn/* || timer < 0*/) return
+            Renderer.drawBox(box, Colors.MINECRAFT_GOLD)
     }
 }
